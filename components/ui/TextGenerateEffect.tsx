4a1f7069ec99e81bfb43f035b3ea89a26a5356ba
Vanshdiyora <vanshadiyora@gmail.com>
2024-07-11 11:22:57 +0530

    third commit

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -11,7 +11,8 @@ export const TextGenerateEffect = ({
   className?: string;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  const wordsArray = words.split(" ");
+
   useEffect(() => {
     animate(
       "span",
@@ -23,7 +24,7 @@ export const TextGenerateEffect = ({
         delay: stagger(0.2),
       }
     );
-  }, [scope.current]);
+  }, [animate]);
 
   const renderWords = () => {
     return (
@@ -32,7 +33,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className={`${idx>3? 'text-purple':'dark:text-white text-black'} opacity-0}`}
+              className={`${idx > 3 ? 'text-purple' : 'dark:text-white text-black'} opacity-0`}
             >
               {word}{" "}
             </motion.span>
@@ -45,7 +46,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-bold", className)}>
       <div className="my-4">
-        <div className=" dark:text-white text-black  leading-snug tracking-wide">
+        <div className="dark:text-white text-black leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
--- data/index.ts
+++ data/index.ts
@@ -173,45 +173,48 @@ export const navItems = [
   export const workExperience = [
     {
       id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      title: "Frontend Engineer Intern at Par Solution",
+      desc: "Developed user interfaces using React.js and Next.js. Created reusable components, optimized performance, and collaborated with teams to deliver responsive web applications.",
       className: "md:col-span-2",
       thumbnail: "/exp1.svg",
     },
-    {
-      id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp2.svg",
-    },
-    {
-      id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
-    },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
+    // {
+    //   id: 2,
+    //   title: "Mobile App Dev - JSM Tech",
+    //   desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp2.svg",
+    // },
+    // {
+    //   id: 3,
+    //   title: "Freelance App Dev Project",
+    //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Lead Frontend Developer",
+    //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
   ];
   
   export const socialMedia = [
     {
       id: 1,
+      link:'https://github.com/Vanshdiyora',
       img: "/git.svg",
     },
     {
       id: 2,
+      link:'',
       img: "/twit.svg",
     },
     {
       id: 3,
+      link:'https://www.linkedin.com/in/vansh-diyora-263996221/',
       img: "/link.svg",
     },
   ];
